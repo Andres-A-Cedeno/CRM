18040580269dd24f56675ed6fbe43bd59fb73ba1
inveligent <inveligentdev303@gmail.com>
2025-01-16 16:46:15 -0500

    Nuevos cambios aplicados

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users/users.services';
 import { MatIcon } from '@angular/material/icon';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-home',
-  imports: [MatIcon],
+  imports: [MatIcon, CdkAccordionModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  items = ['Jessica Espejo', 'David Avila', 'Luis Fernando'];
+  expandedIndex = 0;
+
   constructor(public userService: UsersService) {}
   ngOnInit() {
     this.getUserLogged();
