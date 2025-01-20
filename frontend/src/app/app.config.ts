0e8954492d9c5a96f500e3b3760a6b924de5867d
inveligent <inveligentdev303@gmail.com>
2025-01-20 17:01:58 -0500

    Nuevos cambios

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,18 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection,
+  //provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    //provideZoneChangeDetection(),
     provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
   ],
